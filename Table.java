cc35d2498d088dac9ceb3c9980e1498f2d8c13ff
NamrataGadhave414 <125796178+NamrataGadhave414@users.noreply.github.com>
2023-07-14 11:30:38 +0700

    Update Table.java

--- Table.java
+++ Table.java
@@ -7,10 +7,11 @@ public class Table {
 		int number;  //Declare the variable
 		//Take the input fromm the user Create the input of scanner class
 		Scanner scanner =new Scanner(System.in);
-		System.out.println("Enter a Number:");
+		System.out.println("Enter a Number:");   
 		number=scanner.nextInt();
+		//loop start execution form execute until the condition i<=10 becomes false
 		for(int i=1; i<=10;i++) {
-			System.out.println( number + "*" + i + "=" + number * i );
+			System.out.println( number + "*" + i + "=" + number * i );  //prints table of the entered number
 		}
 
 	}
