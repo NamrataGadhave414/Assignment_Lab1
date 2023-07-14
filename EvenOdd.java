761554f38b2910d1af1a9c16e084ae0686b695ef
NamrataGadhave414 <125796178+NamrataGadhave414@users.noreply.github.com>
2023-07-14 11:47:44 +0700

    Update EvenOdd.java

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,14 +1,17 @@
+//question:Write A Program To Accept A Number From The User And Check It Is Even Or Odd.
+
 package quest3_3_EvenOrOdd;
 
 import java.util.Scanner;
 
 public class EvenOdd {
 	public static void main(String[] args) {
-		int num;
-		Scanner scanner = new Scanner(System.in);
+		int num;  //Decalre The Variable
+		//Taking  the input from the user 
+		Scanner scanner = new Scanner(System.in);  //Create th e input of scanner class
 		System.out.println("Enter A Number");
 		num = scanner.nextInt();
-		if (num%2==0) {
+		if (num%2==0) {  //Checking the condition given number is Even Or Odd
 			System.out.println(num + " It's a Even Number");
 		}else {
 			System.out.println(num + " It's a Odd Number" );
